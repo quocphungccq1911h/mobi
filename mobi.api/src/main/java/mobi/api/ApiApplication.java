1455555168d207ea56e30a7ba354f17e5de46ced
CUN-PC\ADMIN <quocphungccq1911h@gmail.com>
2025-08-26 19:55:11 +0700

    up

--- mobi.api/src/main/java/mobi/api/ApiApplication.java
+++ mobi.api/src/main/java/mobi/api/ApiApplication.java
@@ -1,13 +1,10 @@
 package mobi.api;
 
-import mobi.common.utils.CommonUtils;
-import mobi.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
